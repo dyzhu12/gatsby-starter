3223017d5d8db614520c05603a7232f789d70793
David Zhu <dyzhu12@gmail.com>
2019-02-03 11:56:28 -0500

    Add form

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,17 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <form name="contact" data-netlify="true">
+      <p>
+        <label>Name <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Email <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </Layout>
 )
 
